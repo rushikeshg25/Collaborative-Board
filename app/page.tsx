4b5404b02861b74406410c619242cc978a05c1ca
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-02-01 21:40:51 +0530

    Initialized Convex

--- app/page.tsx
+++ app/page.tsx
@@ -15,15 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+      RUSHIKESH
           </a>
         </div>
       </div>
